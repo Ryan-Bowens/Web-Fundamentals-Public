5dffa56666bd8f540e17f4a125067b3b4d44ec09
Ryan_Bowens <bowensryan@gmail.com>
2023-09-11 18:09:44 -0700

    ButtonClicker Assignment

---
+++ ButtonClicker/script.js
@@ -0,0 +1,16 @@
+//1. Change button to say logout on click
+//2. Remove button when clicked
+//3. Alert "Ninja was liked" when clicking button
+
+//Change to logout
+function changeLog(element){
+    element.innerText = "Logout";
+}
+//Remove button
+function remove(element){
+    element.remove()
+}
+//Alert Like 
+function liked(element){
+    element.HTML = alert("Ninja was liked")
+}
